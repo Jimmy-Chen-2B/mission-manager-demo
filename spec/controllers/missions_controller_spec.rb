bd9268f3a70c416e00cb31b98858a592de0b8f8e
Jimmy-Chen-2B <cyd08067@icloud.com>
2021-10-15 17:32:53 +0800

    add mission#show page

--- spec/controllers/missions_controller_spec.rb
+++ spec/controllers/missions_controller_spec.rb
@@ -1,16 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe MissionsController, type: :controller do
+RSpec.describe MissionsController do
   describe "GET /index" do
-    it "assigns @missions and render template" do
+    it "assigns @missions and render" do
       mission1 = FactoryBot.create(:mission)
       mission2 = FactoryBot.create(:mission)
 
       get :index
 
       expect(assigns[:missions]).to eq([mission1, mission2]) 
-      expect(response).to render_template("index") 
     end
 
+    it "render template" do
+      mission1 = FactoryBot.create(:mission)
+      mission2 = FactoryBot.create(:mission)
+
+      get :index
+
+      expect(response).to render_template("index") 
+    end
+  end
+
+  describe "GET show" do
+    it "assigns @course" do
+    mission =  FactoryBot.create(:mission)
+
+    get :show, params: { id: mission.id }
+
+    expect(assigns[:mission]).to eq(mission)
+    end
+
+    it "assigns @course" do
+      mission =  FactoryBot.create(:mission)
+  
+      get :show, params: { id: mission.id }
+  
+      expect(response).to render_template("show")
+    end
   end
 end
